a2805783322553f177d16a3987399d806c51e098
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-11-16 16:27:21 +0100

    add routing, links and navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavouritesPage from "./pages/Favourites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
