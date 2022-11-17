8f45ec9ac7bcbc7334f58cf36847461a0006e70c
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-11-17 13:54:50 +0100

    add outputting lists of data

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import MeetupItem from "./MeetupItem";
+import styles from "./MeetupList.module.css";
+
+const MeetupList = (props) => {
+  return (
+    <ul className={styles.list}>
+      {props.meetups.map((meetup) => {
+        return (
+          <MeetupItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            description={meetup.description}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default MeetupList;
