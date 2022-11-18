64d3b51d3eccd30e631f5549b8aafbb36bf37deb
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-11-18 09:13:50 +0100

    add wrapper and layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
+
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavouritesPage from "./pages/Favourites";
 import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,23 @@
 import React from "react";
+import Card from "../ui/Card";
 import styles from "./MeetupItem.module.css";
 
 const MeetupItem = (props) => {
   return (
     <li className={styles.item}>
-      <div className={styles.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={styles.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={styles.actions}>
-        <button>Add to favourites</button>
-      </div>
+      <Card>
+        <div className={styles.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={styles.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={styles.actions}>
+          <button>Add to favourites</button>
+        </div>
+      </Card>
     </li>
   );
 };
