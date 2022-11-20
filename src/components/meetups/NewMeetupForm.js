a41ef693b9be1298b309e981e0b9781dddb248a9
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-11-20 11:41:49 +0100

    add sending post request and fetching data

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import styles from "./NewMeetupForm.module.css";
 import Card from "../ui/Card";
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -22,7 +22,7 @@ const NewMeetupForm = () => {
       address: enteredAddress,
       description: enteredDescription,
     };
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   };
 
   return (
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  const navigate = useNavigate();
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://react-meetups-990bc-default-rtdb.europe-west1.firebasedatabase.app//meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
